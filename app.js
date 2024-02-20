fe02fd1c08b4f940105efe1c0c0d6b0de280a9a3
HHH <vinahog514@tupanda.com>
2024-02-21 02:03:19 +0900

    fourth commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let news = [];
 
 const getLatestNews = async() => {
     // const url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`);
-    const url = new URL(`https://noonatimess.netlify.app/top-headlines?`);
+    const url = new URL(`https://noonatimess.netlify.app/top-headlines?&country=kr`);
 
 
     const response = await fetch(url);
