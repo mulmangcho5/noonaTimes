ee47f3136c5bb7077a6c770da31fe072b4af43db
HHH <vinahog514@tupanda.com>
2024-02-21 01:36:34 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const API_KEY = 'f36eca93f670483399b76a2befb0db58'
+let news = [];
+
+const getLatestNews = async() => {
+    const url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`);
+
+    const response = await fetch(url);
+    console.log("rrr", response);
+
+    const data = await response.json();
+    news= data.articles
+
+    console.log("news", news);
+}
+
+
+getLatestNews()
