96526b73221215b468522af5db5e56ecf907b003
HHH <vinahog514@tupanda.com>
2024-02-22 02:29:32 +0900

    render

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const API_KEY = 'f36eca93f670483399b76a2befb0db58'
-let news = [];
+let newsList = [];
 
-const getLatestNews = async() => {
+const getLatestNews = async () => {
     // const url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`);
     const url = new URL(`https://noonatimess.netlify.app/top-headlines?&country=kr`);
 
@@ -10,10 +10,33 @@ const getLatestNews = async() => {
     console.log("rrr", response);
 
     const data = await response.json();
-    news= data.articles
+    newsList = data.articles
 
-    console.log("news", news);
+    console.log("news", newsList);
+    render()
 }
+
+
+const render = () => {
+    let newsHtml = newsList.map(news => `
+    <div class="row news">
+                <div class="col-lg-4">
+                    <img class="news-img-size" src="${news.urlToImage}"
+                        alt="">
+                </div>
+                <div class="col-lg-8">
+                    <h2>${news.title}</h2>
+                    <p>${news.description}</p>
+                    <div>${news.source.name} * ${news.publishedAt}</div>
+                </div>
+            </div>
+    `).join("");
+
+
+    document.querySelector("#news-board").innerHTML = newsHtml;
+}
+
+
 
 
 getLatestNews()
